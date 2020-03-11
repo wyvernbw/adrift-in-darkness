87eb5bb6ceacfc2a36fe2850b7260a50f30e6643
Colin <iamwyvern411@gmail.com>
2020-03-11 07:46:04 -0700

    random commit

--- Scripts/BranchingDialogueBox.gd
+++ Scripts/BranchingDialogueBox.gd
@@ -1,4 +1,12 @@
 extends "res://Scripts/DialogueBox.gd"
 
+onready var option_1 = $Panel/option1
+onready var option_2 = $Panel/option1
+
 func _ready():
-	$Panel/option1.grab_focus()
+	option_1.grab_focus()
+	draw_box("fuck", "don't fuck")
+
+func draw_box(_option_1 : String, _option_2 : String):
+	option_1.get_node("Label").text = _option_1
+	option_2.get_node("Label").text = _option_2
