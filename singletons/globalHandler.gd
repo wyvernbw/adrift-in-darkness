9d6bd2431ab0843f9e883eea77693c2efabe041f
wYvernBW <iamwyvern411@gmail.com>
2021-06-14 14:34:42 +0300

    ambience tweaks

--- singletons/globalHandler.gd
+++ singletons/globalHandler.gd
@@ -2,7 +2,7 @@ extends Node
 
 var do_static_effect : bool = true
 export var min_global_light : float = 0.025
-export var globalLight : float = 0.2
+export var globalLight : float = 0.05
 export var bloodstainsMainHall : bool = false
 export var current_static : float = 0.0
 var lantern_fuel : float = -1 
