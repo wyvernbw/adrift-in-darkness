4a7ed23cd794e8021fa1ed30dc6026972b056186
wYvernBW <iamwyvern411@gmail.com>
2021-06-14 14:22:38 +0300

    fixed a big where doors responded to the dialogue_box_removed signal of other objects

--- objects/door/Door.gd
+++ objects/door/Door.gd
@@ -11,13 +11,13 @@ onready var player_node
 onready var interact_delay_timer : Timer = $InteractDelay
 
 var player_is_colliding : bool
+var dialogue_open : bool
 var player_is_looking : bool
 var required_item : Item
 var can_interact : bool = true
 
 export var scene_name : String
 export var locked : bool 
-export var unlock_on_interact : bool = false
 export var locked_dialogue : Resource
 export var unlocked_text : String
 export var key_name : String 
@@ -38,6 +38,10 @@ func _ready() -> void:
 
 	$CanvasLayer/Sprite.visible = true
 	$CanvasLayer/Sprite/AnimationPlayer.play("fade out")
+	
+	interact_delay_timer.autostart = true
+	can_interact = true
+	interact_delay_timer.start()
 
 func _process(_delta : float) -> void:
 	var interact = Input.is_action_just_pressed("interact")
@@ -51,19 +55,23 @@ func _process(_delta : float) -> void:
 		return
 	if inventory_gui.visible:
 		return
-	if not  player_node.look_dir == required_look_dir:
+	if not player_node.look_dir == required_look_dir:
+		player_is_looking = false
 		return
+	else: 
+		player_is_looking = true
 	if not player_is_colliding :
 		return
 
 	# nothing preventing the player from opening the door
-	if  interact:
+	if interact:
 		print("interacted with door " + str(self))
 		if not locked and scene_name:
 			$CanvasLayer/Sprite/AnimationPlayer.play("fade in")
 			$Sounds/Open.play()
 		elif locked:
 			if InventoryHandler.get_item(required_item) != -1:
+				print("door unlocked")
 				locked = false
 				$Sounds/Unlocked.play()
 				can_interact = true
@@ -74,14 +82,13 @@ func _process(_delta : float) -> void:
 				DialogueHandler.dialogue = locked_dialogue
 				DialogueHandler.page_index += 1 
 				DialogueHandler.add_dialogue_box()
-				# var dialogue_box = DIALOGUE_BOX_SCENE.instance()
-				# varadd_child(dialogue_box)
-				# vardialogue_box.get_node("Panel/Label").bbcode_text = locked_text
-				# varDialogueHandler.emit_signal("player_pause")
-				# varDialogueHandler.dialogue_open = true
+				dialogue_open = true
 
 func _on_dialogue_box_removed() -> void:
-	start_delay()
+	# can_interact = true
+	if dialogue_open == true:
+		start_delay()
+		dialogue_open = false
 		
 func _on_Door_body_entered(body : Node) -> void:
 	if body is Player:
@@ -95,6 +102,7 @@ func _on_Open_finished():
 	emit_signal("player_entered", scene_name)
 	inventory_gui.refresh_inventory()
 	$CanvasLayer/Sprite/AnimationPlayer.play("fade out")
+	can_interact = true
 
 
 func _on_InteractDelay_timeout() -> void:
