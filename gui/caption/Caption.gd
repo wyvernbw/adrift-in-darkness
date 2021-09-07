cc74c847ea9f314fb32fedfbe700f040b0c1f374
wyvernbw <iamwyvern511@gmail.com>
2021-09-07 16:16:02 +0300

    Add captions for deaf people

---
+++ gui/caption/Caption.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+
+var text : String 
+
+
+func _ready() -> void:
+	$Container/Label.text = text
+	$Timer.wait_time = GlobalHandler.captions_duration
+	$Timer.start()
+
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play("fade_out")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	queue_free()
