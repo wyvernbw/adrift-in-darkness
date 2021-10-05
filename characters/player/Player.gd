a9e7503f774319870319f41ebf852d36286ed1d8
wyvernbw <iamwyvern511@gmail.com>
2021-10-05 18:40:02 +0300

    Fix misuse of ':='

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -32,9 +32,9 @@ On different occasions and events, increase the insanity score. Higher insanity 
 """
 
 func _ready() -> void:
-	GlobalHandler.Player := self
-	StepTimer.wait_time := walk_step_interval
-	$Particles2D.visible := false
+	GlobalHandler.Player = self
+	StepTimer.wait_time = walk_step_interval
+	$Particles2D.visible = false
 
 	step_sounds.append(load("res://characters/player/wood_step1.wav"))
 	step_sounds.append(load("res://characters/player/wood_step2.wav"))
@@ -44,8 +44,8 @@ func _ready() -> void:
 
 func _physics_process(delta : float) -> void:
 	if GlobalHandler.left_arm == false:
-		anim_suffix := "_left_arm"
-		$Particles2D.visible := true
+		anim_suffix = "_left_arm"
+		$Particles2D.visible = true
 	if can_move:
 		_get_input()
 	_calculate_speed()
@@ -59,8 +59,7 @@ func _physics_process(delta : float) -> void:
 		
 	apply_motion()
 	apply_friction()
-	var anim_speed : float = speed / 6
-	change_animation_speed(anim_speed)
+	change_animation_speed(speed / 6)
 	
 	#move body along vector
 	move_and_slide(velocity, Vector2.UP)
@@ -70,20 +69,20 @@ func apply_motion():
 	velocity.y += speed * move_dir.y
 	
 func apply_friction():
-	velocity.x := lerp(velocity.x, 0, deaccel)
-	velocity.y := lerp(velocity.y, 0, deaccel)
+	velocity.x = lerp(velocity.x, 0, deaccel)
+	velocity.y = lerp(velocity.y, 0, deaccel)
 
 func _update_look_dir() -> void:
 	if Input.is_action_pressed("move_right"):
-		look_dir := Vector2.RIGHT
+		look_dir = Vector2.RIGHT
 	if Input.is_action_pressed("move_left"):
-		look_dir := Vector2.LEFT
+		look_dir = Vector2.LEFT
 	if Input.is_action_pressed("move_down"):
-		look_dir := Vector2.DOWN
+		look_dir = Vector2.DOWN
 	if Input.is_action_pressed("move_up"):
-		look_dir := Vector2.UP
+		look_dir = Vector2.UP
 	
-	LookRaycast.cast_to := look_dir * 8
+	LookRaycast.cast_to = look_dir * 8
 	
 func _get_input() -> void:
 	#get keyboard input
@@ -96,32 +95,32 @@ func _get_input() -> void:
 	move_dir.y = int(moving_down) - int(moving_up)
 	
 	if move_dir.x == 0 and move_dir.y == 0:
-		moving := false
+		moving = false
 	else:
-		moving := true
+		moving = true
 
 func _calculate_speed() -> void:
 	if Input.is_action_pressed("sprint"):
 		if stamina > 0 and moving and not staggered :
-			speed := sprint_speed
+			speed = sprint_speed
 			stamina -= 1
-			StepTimer.wait_time := sprint_step_interval
+			StepTimer.wait_time = sprint_step_interval
 		else:
-			speed := BASE_SPEED
-			StepTimer.wait_time := walk_step_interval
+			speed = BASE_SPEED
+			StepTimer.wait_time = walk_step_interval
 	else:
-		speed := BASE_SPEED
+		speed = BASE_SPEED
 		stamina += 1
-		StepTimer.wait_time := walk_step_interval
+		StepTimer.wait_time = walk_step_interval
 	
 	if stamina == 0 and staggered == false:
-		staggered := true
+		staggered = true
 	if stamina == 200 and staggered == true:
-		staggered := false
+		staggered = false
 	if staggered:
-		speed := stagger_speed
+		speed = stagger_speed
 		
-	stamina := clamp(stamina, 0, MAX_STAMINA)
+	stamina = clamp(stamina, 0, MAX_STAMINA)
 	
 	if not moving:
 		stamina += 1
@@ -130,13 +129,13 @@ func play_anim(anim : String, dir : Vector2) -> void:
 	var dir_str : String = "up"
 	match dir:
 		Vector2.RIGHT:
-			dir_str := "right"
+			dir_str = "right"
 		Vector2.LEFT:
-			dir_str := "left"
+			dir_str = "left"
 		Vector2.UP: 
-			dir_str := "up"
+			dir_str = "up"
 		Vector2.DOWN:
-			dir_str := "down"
+			dir_str = "down"
 	
 	$AnimatedSprite.play(anim + dir_str + anim_suffix)
 
@@ -159,10 +158,10 @@ func save_game(game_save : Resource) -> void:
 func load_game(game_save : Resource) -> void:
 	var data: Dictionary = game_save.data[SAVE_KEY]
 	print(data)
-	position := Vector2.ZERO
+	position = Vector2.ZERO
 	get_node('/root/Game').switch_scene(data['current_scene'])
-	position.x := data['position']['x']
-	position.y := data['position']['y']
+	position.x = data['position']['x']
+	position.y = data['position']['y']
 	
 """
 SIGNAL METHODS vvv
@@ -171,29 +170,29 @@ SIGNAL METHODS vvv
 func _on_StepTimer_timeout() -> void:
 	if moving:
 		randomize()
-		var sound_index := int(rand_range(1, 3))
-		$Sounds/Move.stream := step_sounds[sound_index - 1]
+		var sound_index = int(rand_range(1, 3))
+		$Sounds/Move.stream = step_sounds[sound_index - 1]
 		$Sounds/Move.play()
 
 func _on_player_unpaused() -> void:
-	can_move := true
-	can_look := true
-	DialogueHandler.page_index := 0
-	DialogueHandler.dialogue_branching := false
-	DialogueHandler.dialogue_open := false
+	can_move = true
+	can_look = true
+	DialogueHandler.page_index = 0
+	DialogueHandler.dialogue_branching = false
+	DialogueHandler.dialogue_open = false
 
 func _on_player_paused() -> void:
-	can_move := false
-	can_look := false
-	moving := false
-	move_dir := Vector2.ZERO
+	can_move = false
+	can_look = false
+	moving = false
+	move_dir = Vector2.ZERO
 
 func _on_InventoryGUI_inventory_closed() -> void:
-	can_move := true
-	can_look := true
+	can_move = true
+	can_look = true
 
 func _on_InventoryGUI_inventory_opened() -> void:
-	can_move := false
-	can_look := false
-	moving := false
-	move_dir := Vector2.ZERO
+	can_move = false
+	can_look = false
+	moving = false
+	move_dir = Vector2.ZERO
