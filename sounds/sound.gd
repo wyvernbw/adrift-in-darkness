b1eb42beb16958ad41e4016e97dbf071fe79e513
wyvernbw <iamwyvern511@gmail.com>
2022-07-22 23:48:55 +0300

    Add captions to new sound system

--- sounds/sound.gd
+++ sounds/sound.gd
@@ -4,17 +4,20 @@ extends AudioStreamPlayer2D
 Sound class for playing audio files in the game without worrying about loading resources or managing them. Can play globally or at a certain position.
 """
 
+signal finished_loading
+
+const CAPTION = preload('res://gui/caption/Caption.tscn')
+
 onready var global_sound: AudioStreamPlayer = $Global
 
 var sounds := {}
+var subtitles := {}
 
 export var paths: Array
 
 
 func _ready():
 	load_sounds(paths)
-	play_sound("step_grass_1.wav")
-
 
 func load_sounds(load_paths: Array) -> void:
 	for path in load_paths:
@@ -30,20 +33,37 @@ func load_sounds(load_paths: Array) -> void:
 			continue
 		var file_name := dir.get_next()
 		while file_name:
+			var id = file_name.get_basename()
 			if file_name.get_extension() == "wav":
-				sounds[file_name] = load(path + "/" + file_name)
+				sounds[id] = load(path + "/" + file_name)
+				print("loaded %s" % file_name)
+			if file_name.get_extension() == "txt":
+				var file = File.new()
+				error = file.open(path + "/" + file_name, File.READ)
+				subtitles[id] = file.get_as_text()
 				print("loaded %s" % file_name)
 			file_name = dir.get_next()
 		dir.list_dir_end()
+	emit_signal("finished_loading")
 
 
 func play_sound(id: String, source: Node2D = self) -> void:
-	position = source.position
 	if not id in sounds:
 		return
+
+	position = source.position
+	if id in subtitles:
+		var caption := CAPTION.instance()
+		caption.text = subtitles[id]
+		add_child(caption)
+		print(caption)
 	if source == self:
 		global_sound.stream = sounds[id]
 		global_sound.play()
 		return
 	stream = sounds[id]
 	play()
+
+
+func _on_Sound_finished_loading():
+	play_sound("step_grass_1")
